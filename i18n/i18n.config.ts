7b52ec0df96b413b14f88760bec1a0cbedf82a6c
doick <doic2701@gmail.com>
2025-09-15 20:05:56 +0200

    Add Nuxt modules

---
+++ i18n/i18n.config.ts
@@ -0,0 +1,7 @@
+export const i18n = {
+  defaultLocale: 'pl',
+  locales: [
+    { code: 'en', name: 'English', file: 'en.json' },
+    { code: 'pl', name: 'Polski', file: 'pl.json' }
+  ]
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,50 @@
+import { i18n } from "./i18n/i18n.config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  ssr: false,
+  hooks: {},
+  nitro: {
+    preset: 'node-server',
+    storage: {
+      fs: {
+        driver: 'fs',
+        base: './.data'
+      }
+    },
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+    },
+  },
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/fonts',
+    'nuxt-svgo',
+    '@nuxtjs/i18n'
+  ],
+  i18n,
+  svgo: {
+    defaultImport: 'component',
+  },
+  fonts: {
+    defaults: {
+      weights: [400],
+      styles: ['normal'],
+      subsets: ['latin', 'latin-ext'],
+    },
+    provider: 'google',
+    families: [
+      { name: 'Poppins' },
+      { name: 'Bebas Neue' },
+    ],
+  },
 })
