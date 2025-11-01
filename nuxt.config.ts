da581149f9bdea6b2342f433b3a0452446fa4991
doick <doic2701@gmail.com>
2025-11-01 10:01:56 +0100

    Update Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,14 +3,37 @@ import { loadLocales } from './i18n/utils/loadLocales'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  ssr: false,
   hooks: {},
   nitro: {
-    preset: 'node-server',
-    storage: {
-      fs: {
-        driver: 'fs',
-        base: './.data'
+    preset: 'static',
+    compressPublicAssets: true,
+    minify: true,
+    publicAssets: [
+      {
+        maxAge: 60 * 60 * 24 * 30,
+      },
+    ],
+    routeRules: {
+      '/': { prerender: true },
+      '/_ipx/q_80/**': {
+        headers: {
+          'cache-control': 'public, max-age=31536000, immutable',
+        },
+      },
+      '/_fonts/**': {
+        headers: {
+          'cache-control': 'public, max-age=31536000, immutable',
+        },
+      },
+      '/*.webp': {
+        headers: {
+          'cache-control': 'public, max-age=31536000, immutable',
+        },
+      },
+      '/**': {
+        headers: {
+          'cache-control': 'public, max-age=3600, must-revalidate',
+        },
       },
     },
   },
@@ -24,39 +47,47 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      ],
     },
   },
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
   modules: [
-    '@nuxt/fonts',
     'nuxt-svgo',
     '@nuxtjs/i18n',
     '@nuxt/image',
     '@nuxt/ui',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
   ],
   i18n: {
     locales: loadLocales(),
     strategy: 'no_prefix',
     defaultLocale: 'pl',
     vueI18n: './i18n/i18n.config.ts',
+    detectBrowserLanguage: false,
   },
-  fonts: {
-    defaults: {
-      weights: [400],
-      styles: ['normal'],
-      subsets: ['latin', 'latin-ext'],
+  image: {
+    format: ['webp'],
+    screens: {
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+      '2xl': 1536,
     },
-    provider: 'google',
-    families: [
-      { name: 'Poppins' },
-      { name: 'Bebas Neue' },
-    ],
+    dir: 'assets/img',
+    ipx: {
+      maxAge: 31536000,
+    },
   },
   svgo: {
     defaultImport: 'component',
+  },
+  vite: {
+    build: {
+      sourcemap: false,
+    },
   },
 })
